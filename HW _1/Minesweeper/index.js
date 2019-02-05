d26d790bb0fc5a0c67586bdd8353c1b7d19dc5f0
Christian <c@ctoverton.com>
2019-02-05 12:11:40 -0500

    Minesweeper Recursion

--- HW _1/Minesweeper/index.js
+++ HW _1/Minesweeper/index.js
@@ -1,6 +1,8 @@
 // ========== [ Globals ] ==========
-var mines = 10;
+var minePercentage = 20;
 var boardSize = 100;
+
+var firstClick = true;
 
 // ========== [ Events ] ==========
 
@@ -8,6 +10,8 @@ var boardSize = 100;
 $(document).ready(function(){
     generate();
 });
+
+// ========== [ Functions ] ==========
 
 function generate() {
 
@@ -21,7 +25,74 @@ function generate() {
             cell.className += 'grid-cell';
             $('.grid-row')[i].append(cell);
         }
-
     }
 
+    $('.grid-cell').click(function() {
+        sweep($(this));
+    });
+
+}
+
+function addMines(){
+    $.each($('.grid-cell'), function(){
+        var chance = Math.floor(Math.random()*100);
+
+        if ((chance <= minePercentage) && !($(this).hasClass('start'))) {$(this).addClass('mine')}
+    });
+}
+
+function sweep(cell) {
+    if (firstClick) {
+        firstClick = false;
+        cell.addClass('start');
+        addMines();
+    }
+
+    if (!(cell.hasClass('sweeped'))){
+        cell.gridRow = cell.parent().index();
+        cell.gridCell = cell.index();
+        if (cell.hasClass('mine')) {
+            cell.isMine = true;
+            end();
+        } else {
+            console.log('[%s,%s] %s', cell.gridRow, cell.gridCell, cell.isMine);
+
+            var adjacent = [
+                [cell.gridRow - 1, cell.gridCell -1], [cell.gridRow - 1, cell.gridCell], [cell.gridRow - 1, cell.gridCell +1],
+                [cell.gridRow, cell.gridCell -1], [cell.gridRow, cell.gridCell +1],
+                [cell.gridRow + 1, cell.gridCell -1], [cell.gridRow + 1, cell.gridCell], [cell.gridRow + 1, cell.gridCell +1],
+            ];
+
+            var count = 0;
+
+            for (var i=0; i<adjacent.length; i++) {
+                var row = adjacent[i][0];
+                var col = adjacent[i][1];
+
+                if ((row >= 0)&&(col >= 0) && (row < Math.sqrt(boardSize))&&(col < Math.sqrt(boardSize))) {
+                    var checkCell = $('.grid-row').eq(row).find('.grid-cell').eq(col);
+
+                    if (checkCell.hasClass('mine')){count++}
+                }
+            }
+            cell.addClass('count'+ count);
+            cell.addClass('sweeped');
+            console.log('Count: %s', count);
+            if (count == 0){
+                for (var i=0; i<adjacent.length; i++) {
+                    var nextCell = $('.grid-row').eq(adjacent[i][0]).find('.grid-cell').eq(adjacent[i][1]);
+                    if (((adjacent[i][0] >= 0)&&(adjacent[i][1] >= 0) && (adjacent[i][0] < Math.sqrt(boardSize))&&(adjacent[i][1] < Math.sqrt(boardSize)))) {
+                        console.log(nextCell);
+                        sweep(nextCell);
+
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+function end() {
+    console.log('End');
 }
