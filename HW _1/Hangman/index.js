717d7b33f19d7a226526f8868c0d1c90c71aac2b
Christian Overton <c@ctoverton.com>
2019-02-04 19:22:53 -0500

    SVGs Refined

--- HW _1/Hangman/index.js
+++ HW _1/Hangman/index.js
@@ -80,20 +80,25 @@ $(document).ready(function(){
     start();
     $('.modal').modal();
 
-    var draw = SVG('drawing').size('100%', '100%');
-    var ship = draw.group();
-    ship.polygon('34, 76, 34, 0, 0, 90, 34, 76').fill('#d8d5f4');
-    ship.polygon('34, 76, 34, 0, 68, 90, 34, 76').fill('#B0B0C6');
-    ship.scale(0.5, 0.5);
-
-    console.log(draw);
+    var draw = SVG('drawing').size('100%', '400');
 
     for (var i=0; i<stars; i++) {
         var sx = Math.floor(Math.random()*draw.node.clientWidth);
-        var sy = Math.floor(Math.random()*draw.node.clientHeight);
+        var sy = Math.floor(Math.random()*(draw.node.clientHeight*0.75));
         var ssize = Math.floor(Math.random()*starSize);
         draw.circle(ssize).fill('#d8d5f4').move(sx,sy);
+        //console.log('x: %s y: %s size: %s height: %s', sx, sy, ssize, draw.node.clientHeight);
     }
+
+    var ship = draw.group();
+    ship.polygon('32 0 0 96 32 80 32 0').fill('#d8d5f4');
+    ship.polygon('32 0 64 96 32 80 32 0').fill('#b0b0c6');
+    //ship.scale(0.5, 0.5);
+    ship.center(draw.node.clientWidth/2,352);
+
+    console.log(ship);
+
+
 
 
 });
